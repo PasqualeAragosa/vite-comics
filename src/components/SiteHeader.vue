db317a60880ef7e5731cdc345b3a798c7d8de27b
Pasquale Aragosa <aragosap@gmail.com>
2022-11-10 15:22:59 +0100

    add bootstrap

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    name: 'SiteHeader',
+    data() {
+        return {
+            menu: [
+                {
+                    text: 'Characters',
+                    href: '#'
+                },
+                {
+                    text: 'Comics',
+                    href: '#'
+                },
+                {
+                    text: 'Characters',
+                    href: '#'
+                },
+                {
+                    text: 'Characters',
+                    href: '#'
+                },
+                {
+                    text: 'Characters',
+                    href: '#'
+                },
+                {
+                    text: 'Characters',
+                    href: '#'
+                },
+                {
+                    text: 'Characters',
+                    href: '#'
+                },
+                {
+                    text: 'Characters',
+                    href: '#'
+                },
+            ]
+        }
+    }
+}
+</script>
+<template></template>
+<style>
+
+</style>
